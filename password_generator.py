612e4ed18c3fd484566de69273701ee2dee66f60
Люда <leonbarsia@yandex.ru>
2026-04-15 23:03:40 +0300

    New commit

---
+++ password_generator.py
@@ -0,0 +1,82 @@
+import random
+import string
+
+lowercase_letters = string.ascii_lowercase
+cappital_letters = string.ascii_uppercase
+all_letters = string.ascii_letters
+digits = string.digits
+punctuation = string.punctuation
+
+def random_generate():   
+    while True:
+        try:
+            lenth = int(input("Enter length"))
+            if lenth <= 0:
+                continue
+            break
+        except ValueError:
+            print("Numbers only")
+            continue
+    all =  lowercase_letters + cappital_letters + digits + punctuation
+    for i in range(lenth):
+        generate = random.choice(all)
+        print(generate, end = '')
+
+def choise_generate():
+    while True:
+        try:
+            lenth = int(input("Enter length"))
+            if lenth <= 0:
+                continue
+            break
+        except ValueError:
+            print("Numbers only")
+            continue
+    print(" letters and numbers - 1, numbers and special characters - 2, special characters and letters - 3, only numbers - 4, only special characters - 5, only letters - 6 ")
+    way = int(input("Select a password creation method"))
+    if way == 1:
+        first = all_letters + digits
+        for i in range(lenth):
+            generate_first = random.choice(first)
+            print(generate_first, end = '')
+    if way == 2:
+        second = digits + punctuation
+        for i in range(lenth):
+            generate_first = random.choice(second)
+            print(generate_first, end = '')
+    if way == 3:
+        thidly = digits + punctuation
+        for i in range(lenth):
+            generate_first = random.choice(thidly)
+            print(generate_first, end = '')
+    if way == 4:
+        for i in range(lenth):
+            generate_first = random.choice(digits)
+            print(generate_first, end = '')
+    if way == 5:
+        for i in range(lenth):
+            generate_first = random.choice(punctuation)
+            print(generate_first, end = '')
+    if way == 6:
+        for i in range(lenth):
+            generate_first = random.choice(all_letters)
+            print(generate_first, end = '')
+
+                
+while True:
+    print()
+    try:
+        call = int(input("Select a password creation method (Custom = 1, All characters together = 2) Press 0 to exit the program "))
+    except ValueError:
+        print("Only Numbers")
+        continue
+    if call == 2:
+        random_generate()
+    elif call == 1:
+        choise_generate()
+    elif call == 0:
+         break
+
+
+
+ 
